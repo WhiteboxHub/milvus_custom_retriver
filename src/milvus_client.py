8dceba0af687d23432ec7939e9ffa5a635fc77be
khajamdajmeer <khajamdajmeer@gmail.com>
2025-03-20 15:13:55 -0700

    wrote the pytest for milvus_insert with failed result and for milvus initialization and successfully pased successfully

--- src/milvus_client.py
+++ src/milvus_client.py
@@ -8,9 +8,11 @@ from pymilvus import (connections,
 
 from dotenv import load_dotenv
 import os
-from src.utils import logger
+try:
+    from src.utils import logger
+except ImportError:
+    from utils import logger
 import threading
-from langchain_milvus import Milvus
 
 
 load_dotenv()
@@ -25,37 +27,45 @@ def milvus_initialization(collection_name = None,drop_collection = False):
     with _mivus_thread:
         loggers = logger.Logger()
 
+        connections.connect('default',host=os.getenv("MILVUS_HOST","localhost"),port=os.getenv("MILVUS_PORT","19530"))
+        _milvus_connection = True
         if collection_name is None:
             collection_name = os.getenv('DB_COLLECTION_NAME')
             if not collection_name:
                 loggers.error("unable to find collection_name from env varible ","milvus_init")
                 raise ValueError("collection name not found in env variables...")
-            
 
 
-        if _milvus_connection:
+        if _milvus_connection and not drop_collection:
             try:
                 if connections.has_connection("default"):
                     loggers.log("connection already exists and working ","milvus_init")
-                    return _milvus_collection
+                    if utility.has_collection(collection_name):
+                        _milvus_collection = True
+                        loggers.log(f'milvusdb collection {collection_name} already exists','milvus_init')
+                        return _milvus_collection
+                    else:
+                        print("----------------collection does not have has --------------------")
+                        Collection(collection_name,schema)
+                        _milvus_collection = True
+                        loggers.log(f'milvusdb collection {collection_name} created and retrived','milvus_init')
+                        return _milvus_collection
             except Exception as e:
                 loggers.error(f"error while checking milvus connection {e}","milvus_init")
 
         try:
-            connections.connect('default',host=os.getenv("MILVUS_HOST","localhost"),port=os.getenv("MILVUS_PORT","19530"))
-            _milvus_connection = True
+            # connections.connect('default',host=os.getenv("MILVUS_HOST","localhost"),port=os.getenv("MILVUS_PORT","19530"))
+            
             loggers.log("new milvus connection extablished  ","milvus_init")
         except Exception as e:
             loggers.error(f"error while checking milvus connection {e}","milvus_init")
 
         if drop_collection:
             try:
-                utility.drop_collection(collection_name)
+                utility.drop_collection(collection_name=collection_name)
                 loggers.log('Deleting the collection.','milvus_init')
             except Exception as e:
                 loggers.log(f'unable to delete the collection {e}','milvus_init')
-    
-
 
         fields = [
             FieldSchema(name='pk', dtype=DataType.INT64, is_primary=True, auto_id= True),
@@ -64,17 +74,19 @@ def milvus_initialization(collection_name = None,drop_collection = False):
             FieldSchema(name='text', dtype=DataType.VARCHAR , max_length = 500), 
         ]
 
-
         schema = CollectionSchema(fields,"Machine learning")
 
         try:
             
             if utility.has_collection(collection_name):
-                _milvus_collection = Collection(collection_name)
-                loggers.log(f'milvusdb collection {collection_name} retrived','milvus_init')
+                _milvus_collection = True
+
+                loggers.log(f'milvusdb collection {collection_name} already exists','milvus_init')
                 return _milvus_collection
             else:
-                _milvus_collection = Collection(collection_name,schema)
+                print("----------------collection does not have has --------------------")
+                Collection(collection_name,schema)
+                _milvus_collection = True
                 loggers.log(f'milvusdb collection {collection_name} created and retrived','milvus_init')
                 return _milvus_collection
         except Exception as e:
@@ -83,16 +95,9 @@ def milvus_initialization(collection_name = None,drop_collection = False):
             
         
         
-
-# milvus_initialization(drop_collection=True)
-# print(milvus_initialization())
-def milvus_insert_data(file_name : str,doc : str, doc_embeding):
+def milvus_insert_data(file_name : str,doc : str,doc_embeding):
     logs = logger.Logger()
     try:
-        client = MilvusClient(
-            uri= os.getenv("MILVUS_URI"),
-            token = "root:Milvus"
-        )
 
         data = [{
             "filename":file_name,
@@ -100,16 +105,23 @@ def milvus_insert_data(file_name : str,doc : str, doc_embeding):
             "embeddings":doc_embeding
         }]
 
-        res = client.insert(
-            collection_name=os.getenv("DB_COLLECTION_NAME"),
-            data=data
-        )
-        logs.log("inserted in to the collection","milvus_insert")
-        return res 
+        collection_name= os.getenv("DB_COLLECTION_NAME")
+        if not collection_name:
+            raise ValueError("DB_COLLECTION_NAME is not set in environment variables.")
+        # connections.connect(alias='default',host=os.getenv("MILVUS_HOST","localhost"),port=os.getenv("MILVUS_PORT","19530"))
+        connections.connect('default',host=os.getenv("MILVUS_HOST","localhost"),port=os.getenv("MILVUS_PORT","19530"))
+        print(connections.list_connections()) 
+        if utility.has_collection(collection_name):
+            client = Collection(collection_name)
+            res = client.insert(data)
+            logs.log("inserted in to the collection","milvus_insert")
+            return res 
+
+        else:
+            raise Exception
     except Exception as e:
         logs.error(f"error occured while instering {e}","milvus_insert")
+        raise Exception
 
 
 
-
-   
--- tests/test_milvus.py
+++ tests/test_milvus.py
@@ -3,14 +3,14 @@ from dotenv import load_dotenv
 from unittest.mock import MagicMock, patch
 from pymilvus import (connections, 
                       utility, 
-                      Collection
-                      ,FieldSchema,
+                      Collection,
+                      FieldSchema,
                       DataType,
                       CollectionSchema,
                       MilvusClient)
 from src.milvus_client import milvus_initialization,milvus_insert_data
 import os
-
+import numpy as np
 
 load_dotenv()
 
@@ -23,20 +23,26 @@ MILVUS_URI = os.getenv('MILVUS_URI')
 
 @pytest.fixture
 def mock_milvus(mocker):
+
     '''Mock Milvus connections and collections'''
 
-    mock_connect = mocker.patch("pymilvus.connections.connect")
-    mock_client = mocker.patch(MilvusClient,autospec=True,return_value = True)
+    mock_connect = mocker.patch("pymilvus.connections.connect",autospec=True)
     mock_has_connection = mocker.patch.object(connections, "has_connection",return_value = True)
+    mock_has_connect = mocker.patch.object(connections, "connect",return_value = True)
     mock_has_collection = mocker.patch.object(utility, "has_collection",return_value = True)
     mock_collection = mocker.patch("pymilvus.Collection",autospec = True)
+    mock_drop_collection = mocker.patch.object(utility,"drop_collection",return_value = True)
+    mock_milvus_client = mocker.patch("src.milvus_client.milvus_initialization", autospec=True)
     mock_collection.return_value = MagicMock() 
+
     return {
-        "milvus_client" : mock_client,
         "mock_connect" : mock_connect,
         "mock_has_connection" : mock_has_connection,
         "mock_has_collection" : mock_has_collection,
-        "mock_collection" : mock_collection
+        "mock_collection" : mock_collection,
+        "mock_milvus_client":mock_milvus_client,
+        "mock_drop_collection" : mock_drop_collection,
+        "mock_milvus_connect" : mock_has_connect
     }
 
 
@@ -44,12 +50,61 @@ def test_milvus_initialization(mock_milvus):
     """Test milvus initialization"""
     global DB_COLLECTION_NAME,MILVUS_HOST,MILVUS_PORT
 
-    collection = milvus_initialization(collection_name="test_collection")
+    collection = milvus_initialization(collection_name="test_collection",drop_collection=False)
 
-    mock_milvus['mock_connect'].assert_called_once_with('default',host=MILVUS_HOST,port=MILVUS_PORT)
-    mock_milvus["milvus_client"].assert_called_once_with(uri=os.getenv("MILVUS_URI"),token="root:Milvus")
-    mock_milvus['mock_has_connection'].assert_called_once_with('default')
-    # mock_milvus['mock_has_collection'].assert_called_once_with('test_collection')
-    mock_milvus["milvus_client"].assert_called_once_with(uri=os.getenv("MILVUS_URI"),token="root:Milvus").load_collection(collection_name="test_collection")
+    # mock_milvus['mock_has_connection'].assert_called_once_with('default')
+    if not mock_milvus['mock_has_connection'].return_value:
+        mock_milvus['mock_connect'].assert_called_once_with('default', host=MILVUS_HOST, port=MILVUS_PORT)
+    mock_milvus['mock_has_collection'].assert_called_once_with("test_collection")
+    
 
     assert collection is not None
+
+def test_milvus_initalization_with_drop_collection(mock_milvus):
+    global DB_COLLECTION_NAME,MILVUS_HOST,MILVUS_PORT
+    collection = milvus_initialization(collection_name="test_collection",drop_collection=True)
+
+    mock_milvus['mock_drop_collection'].assert_called_once_with(collection_name="test_collection")
+    mock_milvus['mock_has_collection'].assert_called_once_with("test_collection")
+    mock_milvus['mock_milvus_connect'].assert_called_once_with('default',host=os.getenv("MILVUS_HOST","localhost"),port=os.getenv("MILVUS_PORT","19530"))
+
+
+    assert collection is not None
+
+
+@pytest.fixture
+def mock_milvus_insert(mocker):
+    '''mock milvus insert and response'''
+    mock_milvus_connection = mocker.patch("pymilvus.MilvusClient",autospec = True)
+    mock_milvus_insert = mocker.patch.object(mock_milvus_connection,"insert",return_value= True)
+    mock_has_connect = mocker.patch.object(connections, "connect",return_value = True)
+
+    return {
+        "mock_milvus_connection" : mock_milvus_connection,
+        "mock_milvus_insert" : mock_milvus_insert,
+        "mock_milvus_connect" : mock_has_connect
+
+    }
+
+
+def test_milvus_insert(mock_milvus_insert):
+
+    '''mock insertion of quries and embedding insertion.'''
+    float_list = np.random.rand(768)
+    data = {
+            "filename":"test file name",
+            "text":"this is a test case doc.",
+            "embeddings":float_list
+        }
+    insertion = milvus_insert_data(file_name= data['filename'],
+                                   doc = data['text'],
+                                   doc_embeding= data["embeddings"])
+                                   
+    mock_milvus_insert['mock_milvus_connect'].assert_called_once_with('default',host=os.getenv("MILVUS_HOST","localhost"),port=os.getenv("MILVUS_PORT","19530"))
+    
+    # mock_milvus_insert["mock_milvus_connection"].assert_called_once_with(uri= os.getenv("MILVUS_URI"),token = "root:Milvus")
+    # mock_milvus_insert["mock_milvus_insert"].assert_called_once_with(collection_name=os.getenv("DB_COLLECTION_NAME"),
+    #         data=[data])
+
+    assert insertion is not None
+    
